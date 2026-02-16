778af67f81c21d5833d30e4a67906d8283a6e2e0
Bibounet <bibou.ytb33@gmail.com>
2026-02-16 17:20:10 +0100

    init nouriture, annimeaux in dictionnary

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("program.cs called!");
+        
+        
+        Variables.Init();
+        Console.WriteLine("nbr de tigres: " + Variables.annimaux["Tigre"]); 
+        Console.WriteLine("nbr de Poule: " + Variables.annimaux["Poule"]);
+        Console.WriteLine("nbr de Coq: " + Variables.annimaux["Coq"]);
+        Console.WriteLine("nbr de Aigle: " + Variables.annimaux["Aigle"]); 
+//nouriture
+        Console.WriteLine("nouriture:viande " + Variables.nouriture["Viande"]);
+    }
+}
