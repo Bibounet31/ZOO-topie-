358a0675aa12159d44e74763cfd561604e806aa1
Bibounet <bibou.ytb33@gmail.com>
2026-02-17 14:26:07 +0100

    added cost to animal class

--- Program.cs
+++ Program.cs
@@ -8,11 +8,10 @@ class Program
         Console.WriteLine("program.cs called!");
 
 
-
 		// create new Animals... (calling Public Animals in Animals.cs right >:3)	
-		Animals tiger = new Animals("Tiger",120,"male",false,false);
-		Animals chicken = new  Animals("Chicken",10,"female",false,false);
-		Animals eagle = new Animals("Eagle",15,"female",false,false);
+		Animals tiger = new Animals("Tiger",120,"male",false,false, 200);
+		Animals chicken = new  Animals("Chicken",10,"female",false,false,200);
+		Animals eagle = new Animals("Eagle",15,"female",false,false,200);
 
 		// uses the objects we created: tiger,chicken..... and calling ShowInfos on it
 		tiger.ShowInfos();
