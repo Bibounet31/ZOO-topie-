a782b3f31eb2eb702bd19fde5799e4bc2bf6fd87
Bibounet <bibou.ytb33@gmail.com>
2026-02-17 15:03:25 +0100

    feat(Food): added food class :3

--- Program.cs
+++ Program.cs
@@ -9,14 +9,23 @@ class Program
 
 
 		// create new Animals... (calling Public Animals in Animals.cs right >:3)	
-		Animals tiger = new Animals("Tiger",120,"male",false,false, 200);
-		Animals chicken = new  Animals("Chicken",10,"female",false,false,200);
-		Animals eagle = new Animals("Eagle",15,"female",false,false,200);
+	    Animals tiger = new Animals("Tiger",120,"male",false,false, 200);
+	    Animals chicken = new  Animals("Chicken",10,"female",false,false,200);
+	    Animals eagle = new Animals("Eagle",15,"female",false,false,200);
+			
+	    // create new Food
+			Food viande = new Food("Viande",5,10.5);
 
 		// uses the objects we created: tiger,chicken..... and calling ShowInfos on it
+		//Animals
 		tiger.ShowInfos();
 		chicken.ShowInfos();
 		eagle.ShowInfos();
+		
+		Console.WriteLine("")
+		
+		///Food
+		viande.ShowInfos();
 
     }
 }
