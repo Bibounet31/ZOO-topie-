0bce6222b721b11e49b0d4c4cd14ec2d1f6f1b4d
Bibounet <bibou.ytb33@gmail.com>
2026-03-10 16:32:00 +0100

    enable/disable logs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,10 @@ class Program
 {
     static void Main()
     {
-		///start menu....
-	    ///Menu.Start();
-	    
+
+	    var logs = false;
 		//random debug from when i was lost, might be usefull sometimes
-        Console.WriteLine("program.cs called!");
+        if (logs) {Console.WriteLine("program.cs called!");}
 		
         
         // init default values
@@ -25,27 +24,32 @@ class Program
         
 		// create new Animals...   -- 	name|age|gender|ispregnant|isalive|cost|illness
 		for (int i = 0; i < defaultTigers; i++)
-			new Animals("Tiger", 120, "male", false, true, 200, "false");
+			new Animals("Tiger", 120, "male", false, true, 0, "false");
 
 		for (int i = 0; i < defaultPandas; i++)
-			new Animals("Panda", 60, "female", false, true, 300, "false");
+			new Animals("Panda", 60, "female", false, true, 0, "false");
 
 		for (int i = 0; i < defaultLions; i++)
-			new Animals("Lion", 95, "male", false, true, 150, "false");
-		
-		
-		foreach (var animal in Animals.AllAnimals)
-			animal.ShowInfos();
+			new Animals("Lion", 95, "male", false, true, 0, "false");
 
-			
+		
 	    // create new Food     --    Name|qty|price
-	    new Food("meat", defaultmeat, 200);
-	    new Food("seeds", defaultseeds, 200);
+	    new Food("meat", defaultmeat, 5);
+	    new Food("seeds", defaultseeds, 2.5);
 
-	    foreach (var food in Food.AllFoods)
-		    food.ShowInfos();
+	    
+	    
+	    if (logs) {
+		    foreach (var animal in Animals.AllAnimals)
+			    animal.ShowInfos();
+		    
+		    foreach (var food in Food.AllFoods)
+			    food.ShowInfos();
+	    }
 
-		Console.WriteLine("");
+
+	    ///start menu....
+	    Menu.Start();
 		
 
     }
